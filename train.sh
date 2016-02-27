7e645cc1e11967a6105762d2882738d037d5bb5c
shibataka000 <chise.alter.pasta@gmail.com>
2016-02-27 19:55:10 +0900

    Add shell script to train whitch is copied from download.sh.

--- train.sh
+++ train.sh
@@ -2,3 +2,4 @@
 #curl http://www.nurs.or.jp/~nagadomi/animeface-character-dataset/data/animeface-character-dataset.zip -o animeface-character-dataset.zip
 curl https://s3-ap-northeast-1.amazonaws.com/shibataka000-myawsbucket/animeface-character-dataset.zip -o animeface-character-dataset.zip
 unzip animeface-character-dataset.zip
+python train.py
