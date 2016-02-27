ffa565f1e6b0b8d80e7cbe8c9f34b4d41bcbfdbd
shibataka000 <chise.alter.pasta@gmail.com>
2016-02-27 18:26:11 +0900

    Add net.py whitch defines NN.

--- recognize.py
+++ recognize.py
@@ -9,23 +9,10 @@ import chainer.functions as F
 import chainer.links as L
 
 import animeface
-
-class MyChain(Chain):
-    def __init__(self):
-        super(MyChain, self).__init__(
-            l1=L.Linear(32*32*3, 100),
-            l2=L.Linear(100, 100),
-            l3=L.Linear(100, 176),
-        )
-
-    def __call__(self, x):
-        h1 = F.relu(self.l1(x))
-        h2 = F.relu(self.l2(h1))
-        y = self.l3(h2)
-        return y
+import net
 
 def recognize():
-    model = L.Classifier(MyChain())
+    model = L.Classifier(net.MyChain())
     # optimizer = optimizers.SGD()
     # optimizer.setup(model)
 
